82e93de8e271efe3b56dbc5664e9f9d119dcdfa6
lionmof <sashbondar@yandex.ru>
2018-06-04 14:59:12 +0300

    ngrx for fitness

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBoDUWUEYZft8EiYkErCqOgR4x5GitmVKc",
+    authDomain: "ng-fitness-f364b.firebaseapp.com",
+    databaseURL: "https://ng-fitness-f364b.firebaseio.com",
+    projectId: "ng-fitness-f364b",
+    storageBucket: "ng-fitness-f364b.appspot.com",
+    messagingSenderId: "865060211235"
+  }
+};
